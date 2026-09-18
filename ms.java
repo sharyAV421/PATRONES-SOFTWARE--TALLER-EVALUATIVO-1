d26b6184d64a8c4b0282cdf03cafec4f0cc4656c
sharyAV421 <shary.velasquez@campusucc.edu.co>
2026-09-18 18:00:17 -0500

    Add FabricaProductoParametrico class implementation

---
+++ ms.java
@@ -0,0 +1,29 @@
+Public class FabricaProductoParametrico implemets CrearReglaDisparo {
+	
+	private final String description; 
+	
+
+	}
+
+
+Public class Fabrica ProductoParametrico implements CrearGeneradorCerrtificado 
+
+  
+    private final String description;
+
+    }
+
+
+Public class FabricaProductoParametrico implemets CrearReglaDisparo {
+	
+	private final String description; 
+	
+
+	}
+
+Public class FabricaProductoParametrico implemets CrearReglaDisparo {
+	
+	private final String description; 
+	
+
+	}
